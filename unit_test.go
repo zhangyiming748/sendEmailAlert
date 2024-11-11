218a741a03ff7556890e2a44548a4175c8f4dccd
zen <hasee>
2024-11-11 18:56:31 +0800

    第6次测试

--- unit_test.go
+++ unit_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-// go test -v -run  TestSend --timeout 5h59m -args ${{ secrets.FROM }} ${{ secrets.TO }} 保活 发自github工作流 ${{ secrets.PASSWORD }} TestSend
+// go test -v -run  TestSend --timeout 5h59m -args ${{ secrets.FROM }} ${{ secrets.TO }} 保活 发自github工作流 ${{ secrets.PASSWORD }}
 // args 从第五个参数开始
 func TestSend(t *testing.T) {
 	args := os.Args
@@ -14,16 +14,16 @@ func TestSend(t *testing.T) {
 		t.Logf("第%d个参数:%s\n", i, v)
 	}
 	info := new(Info)
-	info.SetFrom(args[5])              //${{ secrets.FROM }}
-	tos := strings.Split(args[6], ",") //${{ secrets.TO }}
+	info.SetFrom(args[4])              //${{ secrets.FROM }}
+	tos := strings.Split(args[5], ",") //${{ secrets.TO }}
 	info.SetTo(tos)
-	info.SetSubject(args[7]) //保活
-	info.SetText(args[8])    //发自github工作流
+	info.SetSubject(args[6]) //保活
+	info.SetText(args[7])    //发自github工作流
 	//info.SetImage(args[9])
 	info.SetHost(QQ.SMTP)
 	info.SetPort(QQ.SMTPProt)
 	info.SetUsername(args[5]) //${{ secrets.FROM }}
-	info.SetPassword(args[9]) //${{ secrets.PASSWORD }}
+	info.SetPassword(args[8]) //${{ secrets.PASSWORD }}
 	t.Logf("%+v\n", info)
 	status := info.Send()
 	t.Log(status)
