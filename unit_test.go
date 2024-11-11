f86485fcefed7068af361d68b0a50d905994e62d
zen <hasee>
2024-11-11 19:03:20 +0800

    第8次测试

--- unit_test.go
+++ unit_test.go
@@ -2,7 +2,6 @@ package sendEmailAlert
 
 import (
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -13,18 +12,5 @@ func TestSend(t *testing.T) {
 	for i, v := range args {
 		t.Logf("第%d个参数:%s\n", i, v)
 	}
-	info := new(Info)
-	info.SetFrom(args[5])              //${{ secrets.FROM }}
-	tos := strings.Split(args[6], ",") //${{ secrets.TO }}
-	info.SetTo(tos)
-	info.SetSubject(args[7]) //保活
-	info.SetText(args[8])    //发自github工作流
-	//info.SetImage(args[9])
-	info.SetHost(QQ.SMTP)
-	info.SetPort(QQ.SMTPProt)
-	info.SetUsername(args[5]) //${{ secrets.FROM }}
-	info.SetPassword(args[9]) //${{ secrets.PASSWORD }}
-	t.Logf("%+v\n", info)
-	status := info.Send()
-	t.Log(status)
+	t.Log(args)
 }
