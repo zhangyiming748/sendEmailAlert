e232c7fd4fe566d2bc0ebf67249f484ed3fd3271
zen <hasee>
2024-11-11 19:11:03 +0800

    第9次测试

--- unit_test.go
+++ unit_test.go
@@ -5,12 +5,27 @@ import (
 	"testing"
 )
 
-// go test -v -run  TestSend --timeout 5h59m -args ${{ secrets.FROM }} ${{ secrets.TO }} 保活 发自github工作流 ${{ secrets.PASSWORD }}
+// go test -v -run  TestSend --timeout 5h59m -args ${{ secrets.PASSWORD }}
 // args 从第五个参数开始
 func TestSend(t *testing.T) {
 	args := os.Args
 	for i, v := range args {
 		t.Logf("第%d个参数:%s\n", i, v)
 	}
+	info := new(Info)
+	info.SetFrom("1914301892@qq.com") //${{ secrets.FROM }}
+	tos := []string{
+		"578779391@qq.com",
+	}
+	info.SetTo(tos)
+	info.SetSubject("保活")
+	info.SetText("发自github工作流")
+	info.SetHost(QQ.SMTP)
+	info.SetPort(QQ.SMTPProt)
+	info.SetUsername("1914301892@qq.com") //${{ secrets.FROM }}
+	info.SetPassword(args[5])             //${{ secrets.PASSWORD }}
+	t.Logf("%+v\n", info)
+	status := info.Send()
+	t.Log(status)
 	t.Log(args)
 }
