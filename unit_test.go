6292ff99c6e916025a7307b119cdc8f31d56e479
zen <hasee>
2024-11-11 18:06:17 +0800

    第1次测试

---
+++ unit_test.go
@@ -0,0 +1,27 @@
+package sendEmailAlert
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+// go test -v -run  TestSend --timeout 5h59m -args ${{ secrets.FROM }} ${{ secrets.TO }} 保活 发自github工作流 ${{ secrets.PASSWORD }} TestSend
+// args 从第五个参数开始
+func TestSend(t *testing.T) {
+	args := os.Args
+	for i, v := range args {
+		t.Logf("第%d个参数:%s\n", i, v)
+	}
+	info := new(Info)
+	info.SetFrom(args[5])              //${{ secrets.FROM }}
+	tos := strings.Split(args[6], ",") //${{ secrets.TO }}
+	info.SetTo(tos)
+	info.SetSubject(args[7]) //保活
+	info.SetText(args[8])    //发自github工作流
+	//info.SetImage(args[9])
+	info.SetHost(QQ.SMTP)
+	info.SetPort(QQ.SMTPProt)
+	info.SetUsername(args[5]) //${{ secrets.FROM }}
+	info.SetPassword(args[9]) //${{ secrets.PASSWORD }}
+}
